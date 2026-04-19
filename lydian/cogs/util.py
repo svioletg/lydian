5b3d56d533eb1b9d3d4253515f59297b35d87798
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-04-18 23:33:55 -0500

    Add emoji to quick embeds in cogs.util

--- lydian/cogs/util.py
+++ lydian/cogs/util.py
@@ -6,16 +6,16 @@ from lydian.const import COLOR_ERR, COLOR_INFO, COLOR_OK, COLOR_WARN
 
 def embed_info(title: str, description: str | None = None) -> Embed:
     """Returns an ``Embed`` with an info icon and the embed color defined by ``const.COLOR_INFO``."""
-    return Embed(title=title, description=description, color=COLOR_INFO)
+    return Embed(title='ℹ️ ' + title, description=description, color=COLOR_INFO)  # noqa: RUF001
 
 def embed_ok(title: str, description: str | None = None) -> Embed:
     """Returns an ``Embed`` with a success icon and the embed color defined by ``const.COLOR_OK``."""
-    return Embed(title=title, description=description, color=COLOR_OK)
+    return Embed(title='✅ ' + title, description=description, color=COLOR_OK)
 
 def embed_warn(title: str, description: str | None = None) -> Embed:
     """Returns an ``Embed`` with a warning icon and the embed color defined by ``const.COLOR_WARN``."""
-    return Embed(title=title, description=description, color=COLOR_WARN)
+    return Embed(title='⚠️ ' + title, description=description, color=COLOR_WARN)
 
 def embed_error(title: str, description: str | None = None) -> Embed:
     """Returns an ``Embed`` with an error icon and the embed color defined by ``const.COLOR_ERR``."""
-    return Embed(title=title, description=description, color=COLOR_ERR)
+    return Embed(title='❌ ' + title, description=description, color=COLOR_ERR)
