e3dbba5c731fbefcb7f320fb1ca08b35c88fd10f
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-04-23 15:04:40 -0500

    Add YTDLSource to cogs.voice

--- lydian/cogs/voice.py
+++ lydian/cogs/voice.py
@@ -1,4 +1,8 @@
 """Voice-related commands."""
+import asyncio
+from typing import Self
+
+import discord
 import yt_dlp
 from discord.ext import commands
 
@@ -22,6 +26,27 @@ ytdl_format_options = {
 
 ytdl = yt_dlp.YoutubeDL()
 
+class YTDLSource(discord.PCMVolumeTransformer):
+    """A ``YoutubeDL``-based audio source to use in voice channels."""
+
+    def __init__(self, source: discord.AudioSource, *, data: dict[str, str], volume: float = 0.5) -> None:
+        super().__init__(source, volume)
+
+        self.data: dict[str, str] = data
+        self.title: str | None = data.get('title')
+        self.url: str | None = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url: str, *, loop: asyncio.AbstractEventLoop | None = None, stream: bool = False) -> Self:
+        """Creates a ``YTDLSource`` from a URL."""
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            data = data['entries'][0]
+
+        filename: str = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, options='-vn'), data=data)
+
 class VoiceCog(commands.Cog):
     """Voice-related commands."""
-
