1ac31612920576ccef2f2214772f6c93686d67cb
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-06-15 18:01:30 -0500

    Add test for help.command_help_embed()

--- tests/test_help.py
+++ tests/test_help.py
@@ -1,10 +1,15 @@
-from typing import Any, Literal
+from typing import TYPE_CHECKING, Any, Literal
 
 import pytest
 from discord.ext.commands import Parameter
 from discord.types.embed import EmbedField
 
 from lydian import help as cmdhelp
+from lydian.const import EMBED_COLOR_INFO, EmojiStr
+from lydian.util import expect
+
+if TYPE_CHECKING:
+    from tests.conftest import SampleCog
 
 
 @pytest.mark.parametrize(('param_kwargs', 'expected'),
@@ -59,3 +64,17 @@ def test_command_param_embed_field(param_kwargs: dict[str, Any], expected: Embed
     desc = 'An example parameter.'
     assert cmdhelp.command_param_embed_field(param) == expected
     assert cmdhelp.command_param_embed_field(param, desc) == expected | {'value': expected['value'] + f'\n{desc}'}
+
+def test_command_help_embed(sample_cog: SampleCog) -> None:
+    embed = cmdhelp.command_help_embed(sample_cog.greet)
+    assert expect(embed.color).value == EMBED_COLOR_INFO
+    # The title should have the cog's name but the cog_name attribute of commands isn't set until that cog is added to
+    # a bot via bot.add_cog(cog), mocking complicates things more than necessary in this case so we just check for None
+    assert embed.title == f'{EmojiStr.INFO} Help: {EmojiStr.GEAR} None: -greet'
+    assert embed.description == '`-greet [name]`\n\nSends a greeting message, optionally using the provided name.' \
+        + '\n\n**Arguments:**'
+
+    arg_field = embed.fields[0]
+    assert arg_field.name == '[name]'
+    assert arg_field.value == 'Type: text (optional)'
+    assert arg_field.inline is False
