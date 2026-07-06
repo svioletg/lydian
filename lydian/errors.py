403e8979f5460129af045ca288e54f6a8f32d777
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-07-06 15:34:04 -0500

    refactor!: remove exception class errors.AssuranceError

--- lydian/errors.py
+++ lydian/errors.py
@@ -2,8 +2,6 @@
 
 class AbortCommand(Exception):  # noqa: N818 ; similar semantics to the built-in StopIteration, seems reasonable
     """Raised to abort a bot command. Silently ignored by the bot's error handler."""
-class AssuranceError(Exception):
-    """Raised when :py:func:`lydian.util.assure` is passed ``False``."""
 class FileSizeLimitError(Exception):
     """Raised when yt-dlp extraction is aborted due to exceeding the configured filesize limit."""
 class MediaQueueLimitError(Exception):
